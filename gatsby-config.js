4af4f7c0f28d8d444bdf10a5d20c7425feb5f2b0
danny <dannyzamora1996@gmail.com>
2020-08-05 15:59:55 -0700

    fist

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-material-ui`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/images`,
+        name: "images",
+      },
+    },],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
+import Layout from "../components/Layout"
+import { Link, graphql } from "gatsby";
 
-export default function Home() {
-  return <div>Hello world!</div>
+import Carousel from "../components/Carousel"
+
+const Home = ({ data }) => {
+  return (
+    <Layout><Carousel images={data.allFile.edges} /></Layout>
+  )
 }
+
+export const query = graphql`
+  query {
+    allFile(filter: {relativeDirectory: {eq: "carousel"}}) {
+        edges {
+          node {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+  }
+`
+export default Home;
