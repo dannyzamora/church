fb65247f29ca285e0aaf9bd66d1e1a8692f799dc
danny <dannyzamora1996@gmail.com>
2020-09-12 23:43:43 -0700

    prod

---
+++ theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+const theme = createMuiTheme({
+
+    palette: {
+        background: {
+            default: '#E8E8E8',
+            paper: "#DCDCDC"
+        }
+    },
+
+
+});
+export default theme
