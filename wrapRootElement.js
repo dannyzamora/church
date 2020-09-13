9666354fcabf1928b32d9a7e00a11bedc70adacb
danny <dannyzamora1996@gmail.com>
2020-09-13 00:18:49 -0700

    im tired

--- wrapRootElement.js
+++ wrapRootElement.js
@@ -2,9 +2,7 @@ import React from "react"
 import { ThemeProvider } from "@material-ui/core/styles"
 
 import theme from "./theme.js"
-const wrapRootElement = ({ element }) => {
+export const wrapRootElement = ({ element }) => {
     console.info(`theme`, theme)
     return <ThemeProvider theme={theme}>{element}</ThemeProvider>
 }
-
-export { wrapRootElement as default }
